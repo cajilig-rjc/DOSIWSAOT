69671b221e2de37428912d19ac0e23d72ad3e44f
rollyloteria <46166210+rollyloteria@users.noreply.github.com>
2019-01-08 17:33:17 +0800

    Update GetStarted_DOSIWSAOT.py
    Remove API Key for public view

--- GetStarted_DOSIWSAOT/GetStarted_DOSIWSAOT.py
+++ GetStarted_DOSIWSAOT/GetStarted_DOSIWSAOT.py
@@ -1,11 +1,11 @@
 import tweepy
 from textblob import TextBlob
 
-consumer_key = 'BoH28mT8PTlEk8gUKgQMe8M10'
-consumer_secret = '5KfzZj6t3yLxoMMvcnIugqTEV2FgBRBm0ySxNdZjiaxRn6BnBZ'
+consumer_key = ''
+consumer_secret = ''
 
-access_token = '1337023771-UDhFNhLbL65EXDGTp0ogF8KlP3rliQXk1AsItaL'
-access_token_secret = 'Bh0o88XYJeib5KSsmrUDvrH8cmXRwu7czWSkTTKtpsPyq'
+access_token = ''
+access_token_secret = ''
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
